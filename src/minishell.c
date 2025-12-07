f2e7ff18e31e1595b355dd9ca46fb34d3a2406d8
Abdo_elbo <aelbouaz@student.codam.nl>
2025-12-07 20:19:51 +0100

    aded signal handler to handle interruption ctrl+c

--- src/minishell.c
+++ src/minishell.c
@@ -3,15 +3,13 @@
 
 // valgrind -s --leak-check=full --show-leak-kinds=definite --track-fds=yes --log-file="valgrind-%p.log" ./minishell
 
-void	sig_handler(int sig, siginfo_t *info, void *ucontext)
+volatile sig_atomic_t	g_signal;
+
+void	sig_handler(int sig)
 {
-	(void)ucontext;
-	(void)info;
-	if (sig == SIGINT)
-	{
-		write(1, "\nhello\n", 8);
-		exit(EXIT_SUCCESS);
-	}
+	g_signal = sig;
+	if (g_signal == SIGINT)
+		write(1, "\n", 1);
 	// else if (sig == CTRL+D)
 	// {
 	// 	write(1, "\nexit\n", 6);
@@ -24,16 +22,23 @@ int	main(void)
 	char				*line;
 	t_token				*lst;
 
+	g_signal = 0;
 	sigemptyset(&sa.sa_mask);
-	sa.sa_sigaction = sig_handler;
-	sa.sa_flags = SA_SIGINFO;
+	sa.sa_handler = sig_handler;
+	sa.sa_flags = 0;
 	sigaction(SIGINT, &sa, NULL);
 	lst = NULL;
-	while (1)
+	while (!g_signal)
 	{
 		line = readline("Ourminishell>");
 		if (line == NULL)
 			break ;
+		if (g_signal == SIGINT)
+		{
+			free(line);
+			free_lst(lst);
+			return (0);
+		}
 		if (*line != '\0')
 			add_history(line);
 		lst = parse_input(line);
