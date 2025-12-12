9c018be6e81cdc8c455cd55428e02522a514ad5a
Abdessamad Elbouazzaoui <aelbouaz@f1r2s9.codam.nl>
2025-12-12 17:08:56 +0100

    edited the sig_handler function, and added the g_signal and initialized it in main

--- include/minishell.h
+++ include/minishell.h
@@ -20,13 +20,13 @@
 # include <signal.h>
 
 
-void        sig_handler(int sig, siginfo_t *info, void *ucontext);
-int	        find_parameter(const char *s1, const char *s2, size_t size);
-t_token     *parse_input(char *line);
-t_token	    *ft_lstlast_2(t_token *lst);
-void	    ft_lstadd_back_2(t_token **lst, t_token *new);
-t_token     *new_node(char *str, int len);
-void        print_lst(t_token *lst);
-int         is_operator(char c);
+void		sig_handler(int sig);
+int			find_parameter(const char *s1, const char *s2, size_t size);
+t_token		*parse_input(char *line);
+t_token		*ft_lstlast_2(t_token *lst);
+void		ft_lstadd_back_2(t_token **lst, t_token *new);
+t_token		*new_node(char *str, int len);
+void		print_lst(t_token *lst);
+int			is_operator(char c);
 
 #endif
--- src/minishell.c
+++ src/minishell.c
@@ -3,15 +3,13 @@
 
 // valgrind -s --leak-check=full --show-leak-kinds=definite --track-fds=yes --log-file="valgrind-%p.log" ./minishell
 
-void	sig_handler(int sig, siginfo_t *info, void *ucontext)
+volatile sig_atomic_t	g_signal;
+
+void	sig_handler(int sig)
 {
-	(void)ucontext;
-	(void)info;
-	if (sig == SIGINT)
-	{
-		write(1, "\nhello\n", 8);
-		exit(EXIT_SUCCESS);
-	}
+	g_signal = sig;
+	if (g_signal == SIGINT)
+		g_signal = 1;
 	// else if (sig == CTRL+D)
 	// {
 	// 	write(1, "\nexit\n", 6);
@@ -25,12 +23,13 @@ int	main(void)
 	t_token				*lst;
 
 	sigemptyset(&sa.sa_mask);
-	sa.sa_sigaction = sig_handler;
-	sa.sa_flags = SA_SIGINFO;
+	sa.sa_handler = sig_handler;
+	sa.sa_flags = 0;
 	sigaction(SIGINT, &sa, NULL);
 	lst = NULL;
 	while (1)
 	{
+		g_signal = 0;
 		line = readline("Ourminishell>");
 		if (line == NULL)
 			break ;
