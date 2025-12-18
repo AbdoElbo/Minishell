0a7300915dd8e89cbac8edbcdef166c304d8a3f0
Haris Konstantinidis <hkonstan@f0r3s16.codam.nl>
2025-12-18 18:42:38 +0100

    deleted some sigaction

--- src/minishell.c
+++ src/minishell.c
@@ -3,21 +3,6 @@
 
 // valgrind -s --leak-check=full --show-leak-kinds=definite --track-fds=yes --log-file="valgrind-%p.log" ./minishell
 
-void	sig_handler(int sig, siginfo_t *info, void *ucontext)
-{
-	(void)ucontext;
-	(void)info;
-	if (sig == SIGINT)
-	{
-		write(1, "\nhello\n", 8);
-		exit(EXIT_SUCCESS);
-	}
-	// else if (sig == CTRL+D)
-	// {
-	// 	write(1, "\nexit\n", 6);
-	// }
-}
-
 int	main(void)
 {
 	struct sigaction	sa;
@@ -25,10 +10,6 @@ int	main(void)
 	t_token				*lst;
 	t_cmds				*cmds;
 
-	sigemptyset(&sa.sa_mask);
-	sa.sa_sigaction = sig_handler;
-	sa.sa_flags = SA_SIGINFO;
-	sigaction(SIGINT, &sa, NULL);
 	lst = NULL;
 	while (1)
 	{
