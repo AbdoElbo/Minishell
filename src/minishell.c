f892e79f3321f64de47cff7794cf2546687dd00d
Haris Konstantinidis <hkonstan@f0r3s16.codam.nl>
2025-12-18 14:29:58 +0100

    Revert "REVERTED edited the sig_handler function, and added the g_signal and initialized it in main"
    This reverts commit 9c018be6e81cdc8c455cd55428e02522a514ad5a.

--- src/minishell.c
+++ src/minishell.c
@@ -3,13 +3,15 @@
 
 // valgrind -s --leak-check=full --show-leak-kinds=definite --track-fds=yes --log-file="valgrind-%p.log" ./minishell
 
-volatile sig_atomic_t	g_signal;
-
-void	sig_handler(int sig)
+void	sig_handler(int sig, siginfo_t *info, void *ucontext)
 {
-	g_signal = sig;
-	if (g_signal == SIGINT)
-		g_signal = 1;
+	(void)ucontext;
+	(void)info;
+	if (sig == SIGINT)
+	{
+		write(1, "\nhello\n", 8);
+		exit(EXIT_SUCCESS);
+	}
 	// else if (sig == CTRL+D)
 	// {
 	// 	write(1, "\nexit\n", 6);
@@ -23,13 +25,12 @@ int	main(void)
 	t_token				*lst;
 
 	sigemptyset(&sa.sa_mask);
-	sa.sa_handler = sig_handler;
-	sa.sa_flags = 0;
+	sa.sa_sigaction = sig_handler;
+	sa.sa_flags = SA_SIGINFO;
 	sigaction(SIGINT, &sa, NULL);
 	lst = NULL;
 	while (1)
 	{
-		g_signal = 0;
 		line = readline("Ourminishell>");
 		if (line == NULL)
 			break ;
